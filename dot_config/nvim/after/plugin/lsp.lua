85f537536db565ea191760331ee1a85f64e9340f
Pedro Henrique Fernandes <28553108+pedrofernandesxyz@users.noreply.github.com>
2024-02-05 20:25:54 -0300

    lsp.lua: add mapping for confirming selection

--- dot_config/nvim/after/plugin/lsp.lua
+++ dot_config/nvim/after/plugin/lsp.lua
@@ -43,6 +43,7 @@ cmp.setup({
     ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
     ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
     ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-w>'] = cmp.mapping.confirm({ select = true }),
     ['<C-Space>'] = cmp.mapping.complete(),
   }),
 })
